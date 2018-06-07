7399356b571c3801f5b56e8c472814b924559f77
Lewis Dominguez <lewisd@zipcoders-MacBook-Pro-96.local>
2018-06-07 15:01:10 -0400

    merging it to dev

---
+++ src/main/java/io/zipcoder/casino/Game.java
@@ -0,0 +1,14 @@
+package io.zipcoder.casino;
+
+public interface Game {
+   // variable of Player type
+    Player player;
+
+    void startGame();
+
+    void endGame();
+
+    void declareWinner();
+
+
+}
