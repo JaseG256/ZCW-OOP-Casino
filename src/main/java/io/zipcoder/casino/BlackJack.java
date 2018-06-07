a9769640d3a1a530f432744055f7bceca6c4d706
Vincent Sima <vincesima@gmail.com>
2018-06-07 16:26:33 -0400

    blackjack start

---
+++ src/main/java/io/zipcoder/casino/BlackJack.java
@@ -0,0 +1,29 @@
+package io.zipcoder.casino;
+
+public class BlackJack {
+int playerHand;
+int dealerHand;
+int currentBet;
+boolean playingBJ = false;
+CardDeck deck = new CardDeck();
+
+public void start(){
+    playingBj = true;
+}
+
+
+while(playingBJ == true){
+    deck.deal();
+    Cons
+
+
+
+    }
+
+
+
+
+
+
+
+}
