20808df63655da31f4b6fabad8f94c2d77f7a27b
Vincent Sima <vincesima@gmail.com>
2018-06-08 09:02:07 -0400

    blackjack .03

--- src/main/java/io/zipcoder/casino/BlackJack.java
+++ src/main/java/io/zipcoder/casino/BlackJack.java
@@ -1,6 +1,6 @@
 package io.zipcoder.casino;
 
-public class BlackJack extends CardGames implements Gamble, Game{
+public class BlackJack extends CardGames {
     int playerHand;
     int dealerHand;
     int currentBet;
@@ -12,41 +12,67 @@ public class BlackJack extends CardGames implements Gamble, Game{
         playingBj = true;
     }
 
-public BlackJack(Player player){
+    public BlackJack(Player player) {
 
-}
+    }
 
 
 
 while(playingBJ ==true)
 
     {
-        currentBet =  Console.numberFromString(Console.askForInput("How much would you like to bet?"));
+        Console.output("Welcome to BlackJack " + player.name + ". I am your dealer Vince and I am here to take your money!");
+        currentBet = Console.numberFromString(Console.askForInput("How much would you like to bet?"));
         deck.deal();
         if (dealerHand == 21) {
             Console.output("Dealer has BlackJack! You lose!");
             loseBet(currentBet);
             currentBet = 0;
+            deck.deal();
         } else if (playerHand == 21) {
-            Console.output("Dealer has BlackJack! You lose!");
-            winBet(currentBet);
+            Console.output("You have BlackJack! You win!");
+            winBet((currentBet * 3) / 2);
             currentBet = 0;
-        }
-
-
-
-
-
-
-        String action = Console.askForInput("Do you want to hit or stay?");
-
-        if (action.equals("hit")) {
+            deck.deal();
 
         }
-        if (action.equals("stay"))
+        while (playerHand < 21) {
+            String action = Console.askForInput("Dealer is showing: " + "You have " + playerHand + ". Please enter 'hit' or 'stay'.");
+            if (action.equals("hit")) {
+                drawCard();
+                cardValue += playerHand;
+                if (playerHand > 21) {
+                    Console.output("You have " + playerHand + " and bust, you suck!");
+                    loseBet(currentBet);
+                    currentBet = 0;
+                }
+            } else if (action.equals("stay")) {
+                break;
+            }
+        }
+        while (dealerHand < 17 && dealerHand < 21) {
+            Console.output("Dealer has " + dealerHand + " and hits.");
+            drawcard();
+            cardValue += dealerHand;
+            if (dealerHand > 21) {
+                Console.output("Dealer has " + dealerHand + " and busts, you win!");
+                winBet(currentBet);
+                currentBet = 0;
+            } else if (dealerHand >= 17 && dealerHand < 21) {
+                Console.output("Dealer has " + dealerHand + " and stands.");
+            }
+        }
+
 
 
     }
 
-
 }
+
+
+
+
+
+
+
+
