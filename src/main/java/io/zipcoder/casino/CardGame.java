4693c4dd0072d029c1ac9a67a5abe76751d8d065
Jason Gibbs <jason_r_gibbs@yahoo.com>
2018-06-09 18:48:37 -0400

    completely abstract CardGame Class#

--- src/main/java/io/zipcoder/casino/CardGame.java
+++ src/main/java/io/zipcoder/casino/CardGame.java
@@ -1,45 +1,36 @@
 package io.zipcoder.casino;
-//
-//public abstract class CardGame implements Game, Gamble {
-//
-//    int playerHand;
-//    int dealerHand;
-//    int currentBet;
-//    Player player;
-//    CardDeck deck;
-//
-//    public CardGame(Player player) {
-//
-//        this.player = player;
-//        this.deck = new deck();
-//
-//    }
-//
-//
-//    public int drawCard() {
-//
-//
-//    }
-//
-//    public void stand() {
-//
-//    }
-//
-//    void startGame();
-//
-//    void endGame();
-//
-//    void declareWinner();
-//
-//    void bet(int currentBet) {
-//
-//    }
-//    void winBet() {
-//
-//    }
-//    void loseBet(Player player) {
-//
-//    }
-//
-//
 
+public abstract class CardGame implements Game, Gamble {
+
+    int playerHand;
+    int dealerHand;
+    int currentBet;
+    Player player;
+    CardDeck deck;
+
+    public CardGame(Player player) {
+
+        this.player = player;
+        this.deck = new CardDeck();
+
+    }
+
+
+    public abstract int drawCard();
+
+    public abstract void stand();
+
+    public abstract void startGame();
+
+    public abstract void endGame();
+
+    public abstract void declareWinner();
+
+    public abstract void bet(int currentBet);
+
+    public abstract void winBet();
+
+    public abstract void loseBet(Player player);
+
+
+}
